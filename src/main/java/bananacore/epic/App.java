b942827b465519f325442b8ad25baedf3d4eb4c6
Eivind Grimstad <egrimstad95@gmail.com>
2016-02-10 13:22:33 +0100

    Add simple JavaFX skeleton.

--- src/main/java/bananacore/epic/App.java
+++ src/main/java/bananacore/epic/App.java
@@ -1,7 +1,28 @@
 package bananacore.epic;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = null;
+        try {
+            root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main.fxml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }

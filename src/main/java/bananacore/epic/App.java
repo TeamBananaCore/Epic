b7517ba643f8f7b1c7ae136f7374a345ed213124
Eivind <egrimstad95@gmail.com>
2016-02-20 00:13:10 +0100

    Add logger library

--- src/main/java/bananacore/epic/App.java
+++ src/main/java/bananacore/epic/App.java
@@ -5,16 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class App extends Application{
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        logger.debug("App started");
         Pane root = null;
         try {
             root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main.fxml"));

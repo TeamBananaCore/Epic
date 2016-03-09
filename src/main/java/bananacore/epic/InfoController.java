eef37b2bf50482a922960161f79b0deebdc348c9
Sander <hanakmumakil23@gmail.com>
2016-03-09 11:21:28 +0100

    APP9 - Adds settings-possiblity, ready for test w/ JSON-parser

--- src/main/java/bananacore/epic/InfoController.java
+++ src/main/java/bananacore/epic/InfoController.java
@@ -2,19 +2,26 @@ package bananacore.epic;
 
 
 import bananacore.epic.interfaces.SpeedInterface;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class InfoController implements SpeedInterface{
 
     private Timestamp lastTime;
     private boolean displayFuel = true;
-    private boolean displaySpeed = true;
+    private boolean displaySpeed = false;
     private int switchDelay = 2000;
 
     @FXML
@@ -24,7 +31,7 @@ public class InfoController implements SpeedInterface{
     AnchorPane speed;
 
     @FXML
-    BorderPane infoSettingsPane;
+    GridPane infoSettingsPane;
 
     @FXML
     CheckBox displayFuelCheckbox;
@@ -35,31 +42,44 @@ public class InfoController implements SpeedInterface{
     @FXML
     Button saveSettingsButton;
 
-    public void infoSettings(){
-        infoSettingsPane.visibleProperty().set(true);
+    @FXML
+    Button infoSettingsButton;
 
-    }
+    @FXML
+    Spinner<Integer> intervalSpinner;
 
-    public void updateSettings(){
-        displayFuel = displayFuelCheckbox.isSelected();
-        displaySpeed = displaySpeedCheckbox.isSelected();
+    public void saveSettings(){
+        if (displayFuel){
+            fuel.visibleProperty().set(displayFuel);
+        } else if (displaySpeed){
+            speed.visibleProperty().set(displaySpeed);
+        }
+        switchDelay = intervalSpinner.getValue();
         infoSettingsPane.visibleProperty().set(false);
+        infoSettingsButton.visibleProperty().setValue(true);
     }
 
     public void initialize(){
+        intervalSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(2,10));
+        displayFuelCheckbox.selectedProperty().set(true);
+        displaySpeedCheckbox.selectedProperty().set(false);
+        infoSettingsButton.visibleProperty().set(false);
+        fuel.visibleProperty().set(false);
         speed.visibleProperty().set(false);
+        displayFuelCheckbox.selectedProperty().addListener(observable -> {
+            displayFuel = displayFuelCheckbox.selectedProperty().get();
+        });
+        displaySpeedCheckbox.selectedProperty().addListener(observable -> {
+            displaySpeed = displaySpeedCheckbox.selectedProperty().get();
+        });
+
     }
 
-    public void updateDisplay(boolean displayFuel, boolean displaySpeed, int switchDelay){
-        this.displayFuel = displayFuel;
-        this.displaySpeed = displaySpeed;
-        this.switchDelay = switchDelay;
-        if(!displayFuel){
-            fuel.visibleProperty().set(false);
-        }
-        if(!displaySpeed){
-            fuel.visibleProperty().set(false);
-        }
+    public void infoSettings(){
+        fuel.visibleProperty().set(false);
+        speed.visibleProperty().set(false);
+        infoSettingsButton.visibleProperty().set(false);
+        infoSettingsPane.visibleProperty().set(true);
     }
 
     @Override
--- src/main/java/bananacore/epic/SpeedController.java
+++ src/main/java/bananacore/epic/SpeedController.java
@@ -2,11 +2,15 @@ package bananacore.epic;
 
 import bananacore.epic.interfaces.SpeedInterface;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 import java.sql.Timestamp;
 
 public class SpeedController implements SpeedInterface {
+
+    @FXML
+    AnchorPane speedPane;
 
     @FXML
     Text speedText;

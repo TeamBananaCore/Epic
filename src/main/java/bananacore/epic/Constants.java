72deb596f7042aa660183ff2058ae16907cf7bc0
Ole-Magnus Pedersen <olemagnp@stud.ntnu.no>
2016-04-06 14:55:36 +0200

    Created a lightsensor class that communicates with the adc

--- src/main/java/bananacore/epic/Constants.java
+++ src/main/java/bananacore/epic/Constants.java
@@ -1,5 +1,7 @@
 package bananacore.epic;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,6 +15,7 @@ public class Constants {
 
     public static Stage PRIMARY_STAGE;
     public static Scene SCENE;
+    public static final GpioController GPIO = GpioFactory.getInstance();
 
     static {
         PARSER = new OurParser();
